336a7efa2313945c5545af7498d0aaf255f5cd76
muhammadsaad91 <saadattari91@gmail.com>
2022-01-31 01:01:20 +0500

    Live time commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 import React, { useState } from "react";
 
+// console.log(curdate);
 const App=()=>{
-    // let count=0;
-
-    const state=useState();
-    const [count,setCount] = useState(0);
-     const click = ()=>{
-         setCount(count+1);
-        // console.log('clicked ' + count++);
-    // console.log(state);
-    
+    let curTime=new Date().toLocaleTimeString();
+    const [time,setTime] = useState(curTime);
+    const clicked =()=>{
+          let curTime=new Date().toLocaleTimeString();
+          setTime(curTime);
     };
-
-
     return(
-    <>
-    <div className="heading-main">
-    <h1>{count}</h1>
-    <button className="btn" onClick={click}>Click on me </button>
-    </div>
-    </>
-);
-console.log(click);
+        <>
+        <div className="heading-main">
+        <h3>{time}</h3>
+        <button className="btn" onClick={clicked}>Click me to get Current Time</button>
+        </div>
+        </>
+    );
+
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import mzpr from './images/mzpr.jpg'
-// import osman from './images/osman.jpg'
-// import ertugrul from './images/ertugrul.jpg'
 import './index.css'
 
 ReactDOM.render(
     <>
-    <h1><App/></h1>
+    <App/>
     </>
     ,document.getElementById('root')
 );
