112d478c3c924a14b2ea2cbbd7bbaf57f3118716
muhammadsaad91 <saadattari91@gmail.com>
2022-01-30 16:13:49 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import './index.css'
+import React, { useState } from "react";
+
+const App=()=>{
+    // let count=0;
+
+    const state=useState();
+    const [count,setCount] = useState(0);
+     const click = ()=>{
+         setCount(count+1);
+        // console.log('clicked ' + count++);
+    // console.log(state);
+    
+    };
 
 
-function Card(props) {
-    return (
-        <>       <div className='cards'>
-            <div className='card'>
-                <img className='image' src={props.src} alt='Loading' />
-                <div className='card-info'>
-                    <h3 className='heading-main'>{props.movieName}</h3>
-                    <span className='card-title'>Original Netflix Series Watch on Link below</span>
-                    <a href={props.link} target='_blank' >
-                        <button className="btn">Watch Now</button>
-                    </a>
-                </div>
-            </div>
-        </div>
-         </>
-
-
-    );
-}
-
-export default Card;
+    return(
+    <>
+    <div className="heading-main">
+    <h1>{count}</h1>
+    <button className="btn" onClick={click}>Click on me </button>
+    </div>
+    </>
+);
+console.log(click);
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Data from './Data'
-import Card from './App'
-import mzpr from './images/mzpr.jpg'
-import osman from './images/osman.jpg'
-import ertugrul from './images/ertugrul.jpg'
+import App from './App'
+// import mzpr from './images/mzpr.jpg'
+// import osman from './images/osman.jpg'
+// import ertugrul from './images/ertugrul.jpg'
 import './index.css'
 
- console.log(Data[2]);
 ReactDOM.render(
-
     <>
-       <h1 className='heading-main'>My Favourite Netflix Series </h1>
-       <h2 className='heading-main'>(SaAd's App)</h2>
-       <div className="main">  
-        <Card src={mzpr}
-        link={Data[0].link}
-        movieName={Data[0].movieName}
-        />
-        <Card src={osman}
-        link={Data[1].link}
-        movieName={Data[1].movieName}
-        />
-        <Card src={ertugrul}
-        link={Data[2].link}
-        movieName={Data[2].movieName}        />
-    <Card src={mzpr}
-        link={Data[0].link}
-        movieName={Data[0].movieName}
-        />
-        <Card src={osman}
-        link={Data[1].link}
-        movieName={Data[1].movieName}
-        />
-   </div> </>
-    , document.getElementById('root')
+    <h1><App/></h1>
+    </>
+    ,document.getElementById('root')
+);
 
-)
