1776ca3b6b89d13013c3d0b45ecf33c157d05d13
muhammadsaad91 <saadattari91@gmail.com>
2022-02-02 02:55:05 +0500

    test

--- src/Buton.jsx
+++ src/Buton.jsx
@@ -1,6 +1,14 @@
-import react from "react";
+import react, { StrictMode } from "react";
 import { useState } from "react/cjs/react.development";
+// StrictMode(Buton);
 const Buton=(props)=>{
+    //use state to make a state for the button on click
+    const [clicked, setClicked]=useState(props.name);
+    //make a function to make the button on click
+    const handleClick=()=>{
+        setClicked("clicked");
+    }
+
 // const [name,setchng]=useState(props.name)
 // const chng =()=>{
 // setchng('This is Checked by You')
@@ -8,7 +16,7 @@ const Buton=(props)=>{
     return(
         <>
         <a href={props.link} target='_blank'>
-        <button>{props.name}</button>
+        <button onClick={handleClick} >{clicked}</button>
         </a>
         </>
     );
